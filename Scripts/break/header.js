042c5f0e0010448a26f7e4f990879e73318a1cb1
Cube <49463634+RuCu6@users.noreply.github.com>
2024-10-08 02:20:31 +0800

    Update header.js

--- Scripts/break/header.js
+++ Scripts/break/header.js
@@ -1,22 +1,22 @@
-// 2024-08-05 19:25
+// 2024-10-08 02:15
 
 const url = $request.url;
 const header = $request.headers;
 const isNetEase = url.includes("/interface") && url.includes(".music.163.com/");
 
 if (isNetEase) {
-  if ($persistentStore.read("开启共享会员") === "是") {
-    if ($persistentStore.read("RuCu6_Music163_MConfigInfo") === undefined || $persistentStore.read("RuCu6_Music163_MConfigInfo") === null) {
-        $notification.post("脚本出错", "参数缺失", "请在插件内填入数据");
-        $done({});
-      } else {
-        header["cookie"] = $persistentStore.read("RuCu6_Music163_Cookie");
-        header["mconfig-info"] = $persistentStore.read("RuCu6_Music163_MConfigInfo");
-        header["user-agent"] = $persistentStore.read("RuCu6_Music163_UserAgent");
-      }
-    } else {
-      $done({});
+  if (
+    $persistentStore.read("RuCu6_Music163_MConfigInfo") === undefined ||
+    $persistentStore.read("RuCu6_Music163_MConfigInfo") === null
+  ) {
+    $notification.post("脚本错误", "参数缺失", "请在插件内填入数据");
+    $done({});
+  } else {
+    header["cookie"] = $persistentStore.read("RuCu6_Music163_Cookie");
+    header["mconfig-info"] = $persistentStore.read("RuCu6_Music163_MConfigInfo");
+    header["user-agent"] = $persistentStore.read("RuCu6_Music163_UserAgent");
   }
+
   $done({ headers: header });
 } else {
   $done({});
