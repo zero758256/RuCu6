53b1eaa8df6dfc8653ec39e01d857d3dc20d7f63
Cube <49463634+RuCu6@users.noreply.github.com>
2024-10-07 15:05:48 +0800

    防止一些沙雕行为

--- Scripts/break/header.js
+++ Scripts/break/header.js
@@ -6,11 +6,16 @@ const isNetEase = url.includes("/interface") && url.includes(".music.163.com/");
 
 if (isNetEase) {
   if ($persistentStore.read("开启共享会员") === "是") {
-    header["cookie"] = $persistentStore.read("RuCu6_Music163_Cookie");
-    header["mconfig-info"] = $persistentStore.read("RuCu6_Music163_MConfigInfo");
-    header["user-agent"] = $persistentStore.read("RuCu6_Music163_UserAgent");
-  } else {
-    $done({});
+    if ($persistentStore.read("RuCu6_Music163_MConfigInfo") === undefined || $persistentStore.read("RuCu6_Music163_MConfigInfo") === null) {
+        $notification.post("脚本出错", "参数缺失", "请在插件内填入数据");
+        $done({});
+      } else {
+        header["cookie"] = $persistentStore.read("RuCu6_Music163_Cookie");
+        header["mconfig-info"] = $persistentStore.read("RuCu6_Music163_MConfigInfo");
+        header["user-agent"] = $persistentStore.read("RuCu6_Music163_UserAgent");
+      }
+    } else {
+      $done({});
   }
   $done({ headers: header });
 } else {
